dbcf49da989eb6f1a3115ec26fdb0c5b0095cffc
AbdullaAmeen <78493193+AbdullaAmeen@users.noreply.github.com>
2024-03-19 21:18:02 +0530

    Added: Integration for tracking

--- init.py
+++ init.py
@@ -6,10 +6,10 @@ def main():
     
     #initiate frame_capture and lowlight scripts
 
-    frame_capture_process = subprocess.Popen(['python', 'frame_capture.py'])
+    frame_capture_process = subprocess.Popen(['python3', 'frame_capture.py'])
 
-    lowlight_process = subprocess.Popen(['python', os.path.join('Zero-DCE', 'lowlight_test.py')])
-
+    lowlight_process = subprocess.Popen(['python3', os.path.join('Zero-DCE', 'lowlight_test.py')])
+    yolo = subprocess.Popen(['python3', 'yoloIntegratedTracker.py'])
     print("Press 'Esc' to stop")
     try:
         # Listen for Escape keypress event
@@ -19,6 +19,7 @@ def main():
 
     frame_capture_process.terminate()
     lowlight_process.terminate()
+    yolo.terminate()
 
 if __name__ == '__main__':
     main()
