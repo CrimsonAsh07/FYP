45a058fcf8686a3b20f447a5d25faed631ad8cb8
Ameen <78493193+AbdullaAmeen@users.noreply.github.com>
2024-04-16 12:51:08 +0530

    Added: test Image

--- RE_ID VAE/model/loss.py
+++ RE_ID VAE/model/loss.py
@@ -47,7 +47,7 @@ def meanA_loss(recon_x, x, mu, logvar, beta=1):
     reconstruction_function = nn.L1Loss(reduction='sum')
       # Adjust max_norm as needed
     
-    MSE = reconstruction_function(recon_x, x)
+    L1 = reconstruction_function(recon_x, x)
     #MSE = 0.001 * MSE
     # https://arxiv.org/abs/1312.6114 (Appendix B)
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
@@ -55,4 +55,4 @@ def meanA_loss(recon_x, x, mu, logvar, beta=1):
     KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
     KLD = torch.sum(KLD_element).mul_(-0.5)
     #print("MSE ", MSE.item(), "KLD ",KLD.item()  )
-    return MSE + beta*KLD
+    return L1 + beta*KLD
