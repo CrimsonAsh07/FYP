2ab9a7c968b9724e9f2d0863eb66958f357c7bb4
AbdullaAmeen <78493193+AbdullaAmeen@users.noreply.github.com>
2024-04-24 12:20:02 +0530

    Added Instant Alerts and Renamed VAE Folder

--- alert.py
+++ alert.py
@@ -4,7 +4,7 @@ import pywhatkit as kit
 def send_whatsapp_message(phone_number, message):
     try:
         now = datetime.datetime.now()
-        kit.sendwhatmsg(f"+{phone_number}", message, now.hour, now.minute + 1)  
+        kit.sendwhatmsg_instantly(phone_number, message,10, True,5)  
         print("Message sent successfully!")
         return True
     except Exception as e:
@@ -12,8 +12,8 @@ def send_whatsapp_message(phone_number, message):
         return False
 
 # Uncomment to test function
-# if __name__ == "__main__":
-#     recipient_number = 'NUMBER' NUMBER with Country Code without +
-#     message = "Hello from your Python script!"
+if __name__ == "__main__":
+    recipient_number = '+917338870517' #NUMBER with Country Code without +
+    message = "Hello from your Python script!"
 
-#     send_whatsapp_message(recipient_number, message)
+    send_whatsapp_message(recipient_number, message)
